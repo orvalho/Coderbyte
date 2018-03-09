99675b4327d07ceca5b8ad1382c2fd40ac98aeb2
orvalho <rasa396@gmail.com>
2018-03-09 15:43:20 +0200

    #4

--- index.js
+++ index.js
@@ -83,3 +83,47 @@ function LetterCapitalize ( str ) {
 }
 
 LetterCapitalize(readline());
+
+
+
+/*********************************************************************************/
+/* #4 Challenge Simple Symbols
+ * Using the JavaScript language, have the function SimpleSymbols(str) take the str
+ * parameter being passed and determine if it is an acceptable sequence by either
+ * returning the string true or false. The str parameter will be composed of + and
+ * = symbols with several letters between them (ie. ++d+===+c++==a) and for the string
+ * to be true each letter must be surrounded by a + symbol. So the string to the left
+ * would be false. The string will not be empty and will have at least one letter. 
+*/
+
+function SimpleSymbols ( str ) { 
+  
+  // checking if first character in string is a letter
+  var pattFirstCharIsLetter = /^[a-z]/i;
+  var resultFirstCharIsLetter = pattFirstCharIsLetter.test(str);
+  
+ // checking if last character in string is a letter
+  var pattLastCharIsLetter = /[a-z]$/i;
+  var resultLastCharIsLetter = pattLastCharIsLetter.test(str);
+
+  // if first or last character in string is a letter, then sequence is unacceptable.
+  // It means that not each letter in string is surrounded by a + symbol.
+  if ( resultFirstCharIsLetter || resultLastCharIsLetter ) {
+    return false;
+  }
+
+  var pattCharIsLetter = /[a-z]/i;
+  var pattCharIsPlus = /\+/;
+  
+  // looping through string and checking if there is a letter not surrounded by a + symbol
+  for ( var i = 1; i <= str.length-2; i++ ) {
+    if ( pattCharIsLetter.test(str.charAt(i)) === true &&
+        ( pattCharIsPlus.test(str.charAt(i-1)) !== true ||
+         pattCharIsPlus.test(str.charAt(i+1)) !== true )) {
+        return false;
+    }
+  }
+  return true;
+}
+
+SimpleSymbols(readline());
