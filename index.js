f239b0c45ea42455f2fc8b4948176ba65d6a1daa
orvalho <rasa396@gmail.com>
2018-03-10 20:13:41 +0200

    #7 Challenge

--- index.js
+++ index.js
@@ -151,7 +151,6 @@ FirstFactorial(readline());
 
 
 /*********************************************************************************/
-
 // #6 Simple Adding Challenge
 /* Using the JavaScript language, have the function SimpleAdding(num) add up all the
 * numbers from 1 to num. For example: if the input is 4 then your program should return
@@ -167,3 +166,25 @@ function SimpleAdding ( num ) {
 }
    
 SimpleAdding(readline());
+
+
+
+/*********************************************************************************/
+// #7 Check Nums Challenge
+/*
+* Using the JavaScript language, have the function CheckNums(num1,num2) take both parameters
+* being passed and return the string true if num2 is greater than num1, otherwise return the
+* string false. If the parameter values are equal to each other then return the string -1.
+*/
+
+function CheckNums ( num1,num2 ) { 
+  if ( num2 > num1 ) {
+      return true;
+  } else if ( num1 > num2 ) { 
+      return false;
+  } else {
+      return "-1";
+  }
+}
+   
+CheckNums(readline());
