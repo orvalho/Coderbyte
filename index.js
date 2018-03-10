05b9ae479da13cd14f4acde882eb40a43071eb6c
orvalho <rasa396@gmail.com>
2018-03-10 19:43:13 +0200

    #6 challenge

--- index.js
+++ index.js
@@ -147,3 +147,23 @@ function FirstFactorial ( num ) {
 }
  
 FirstFactorial(readline());
+
+
+
+/*********************************************************************************/
+
+// #6 Simple Adding Challenge
+/* Using the JavaScript language, have the function SimpleAdding(num) add up all the
+* numbers from 1 to num. For example: if the input is 4 then your program should return
+* 10 because 1 + 2 + 3 + 4 = 10. For the test cases, the parameter num will be any number from 1 to 1000. 
+*/
+
+function SimpleAdding ( num ) { 
+  var result = 0;
+  for ( var i = num; i > 0; i-- ) {
+    result += i;
+  }
+  return result;     
+}
+   
+SimpleAdding(readline());
